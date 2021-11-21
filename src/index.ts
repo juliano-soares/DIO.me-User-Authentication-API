da0eecbd2ce0ce9a8d22c1211782a16fce72f911
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2021-11-21 20:41:08 -0300

    Finalizada a terceira live.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import express from "express";
+import express from 'express';
+import errorHandler from './middlewares/error-handler.middleware';
+import authorizationRoute from './routes/authorization.route';
 import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
@@ -7,9 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(usersRoute);
 app.use(statusRoute);
+app.use(usersRoute);
+app.use(authorizationRoute);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
-})
+});
